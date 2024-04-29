0ee30885043eaaf8386612a4f9004af0a1618318
BimantaraDwiCahyo <27tapai@gmail.com>
2024-04-30 00:07:08 +0700

    JOBSHEET 10 RESTFUL API

--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -2,29 +2,30 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Foundation\Auth\User as Authenticatable;
-use Monolog\Level;
+use Tymon\JWTAuth\Contracts\JWTSubject;
 
-class UserModel extends Authenticatable
+class UserModel extends Authenticatable implements JWTSubject
 {
-    use HasFactory;
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    public function getJWTCustomClaims()
+    {
+        return [];
+    }
+
     protected $table = 'm_user';
-    public $timestamps = false;
     protected $primaryKey = 'user_id';
-    /*
-     *
+
+    /**
+     * The attributes that are mass assignable
      *
      * @var array
      */
-    protected $fillable = [
-        'user_id',
-        'level_id',
-        'username',
-        'nama',
-        'password',
-    ];
+    protected $fillable = ['level_id', 'username', 'nama', 'password'];
 
     public function level(): BelongsTo
     {
